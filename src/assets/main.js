a80c6537d370793cd38535af29579703383e3ffa
Kevin Viricel <kevin.viricel@capgemini.com>
2016-12-16 15:17:50 +0100

    Tasks done

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
+ const reportCardUrl = "https://www.codeschool.com/users/kleww.json";
 
-  // your code will go here
-
+ $.ajax({
+    url: reportCardUrl,
+    dataType: 'jsonp',
+    success: function(response) {
+      const coursesCompleted = response.courses.completed;
+      for (cc of coursesCompleted) {
+        let $course = $("<div></div>");
+        $course.addClass("course");
+        let $title = $(`<h3>${cc.title}</h3>`);
+        let $img = $(`<img src="${cc.badge}" />`);
+        let $a = $(`<a href=${cc.url} target="_blank">See Course</a>`);
+        $a.addClass("btn btn-primary");
+        $course.append($title);
+        $course.append($img);
+        $course.append($a);
+        $("#badges").append($course);
+      }
+    }
+  });
 });
